34020bc8d7700dcb65a8eb942e7cf0fe9ae853d6
Luis <lhlopezm2@gmail.com>
2023-01-25 03:17:59 -0500

    Adjust metrics for internal regions

--- utils/metrics_after_LTR_harvest.py
+++ utils/metrics_after_LTR_harvest.py
@@ -2,13 +2,11 @@ import pandas as pd
 import numpy as np
 from Bio import SeqIO
 from tensorflow.keras import backend as K
+from utils.extra_functions import clustering
 
-def per_domain(df_ltr, df_True, genoma, domain):
-  if domain == 'all':
-    pass
-  else:
-    df_True = df_True[df_True['Domain']==domain]
-    df_ltr = df_ltr[df_ltr['Class']==domain]
+def per_domain(df_ltr, df_True, genoma):
+  dist_max = 4000
+  dom_min = 4
   ids_pred = df_ltr.index.unique().tolist()
   ids_ground = df_True.index.unique().tolist()
   ids_union = [value for value in ids_pred if value in ids_ground]
@@ -25,14 +23,17 @@ def per_domain(df_ltr, df_True, genoma, domain):
       Y_true_end = np.array([Y_true_end])
     for i in range(Y_true_start.shape[0]):
       Y_true_nt[Y_true_start[i]:Y_true_end[i]]=1
-    Y_pred_start = np.array(df_ltr.loc[id]['real_start'])
-    Y_pred_end = np.array(df_ltr.loc[id]['real_end'])
+    Y_pred = df_ltr.loc[id].copy()
+    Y_pred_start = np.array(Y_pred['Start'])
+    Y_pred_end = Y_pred_start + np.array(Y_pred['Length'])
     Y_pred_nt = np.zeros((size))
     if Y_pred_start.size == 1:
       Y_pred_start = np.array([Y_pred_start])
       Y_pred_end = np.array([Y_pred_end])
-    for i in range(Y_pred_start.shape[0]):
-      Y_pred_nt[Y_pred_start[i]:Y_pred_end[i]]=1
+    indices2 = [(Y_pred_start[i],Y_pred_end[i]) for i in range(len(Y_pred_start))]
+    doms = clustering(A=indices2, dist_max=dist_max, dom_min=dom_min)
+    for tupla in doms:
+      Y_pred_nt[tupla[0]:tupla[1]]=1
     TP_window = np.sum(Y_true_nt*Y_pred_nt)
     TP += TP_window
     FP += np.sum(Y_pred_nt)-TP_window
@@ -41,14 +42,17 @@ def per_domain(df_ltr, df_True, genoma, domain):
 
   for id in ids_pred_unique:
     size = len(genoma[id])
-    Y_pred_start = np.array(df_ltr.loc[id]['real_start'])
-    Y_pred_end = np.array(df_ltr.loc[id]['real_end'])
+    Y_pred = df_ltr.loc[id].copy()
+    Y_pred_start = np.array(Y_pred['Start'])
+    Y_pred_end = Y_pred_start + np.array(Y_pred['Length'])
     Y_pred_nt = np.zeros((size))
     if Y_pred_start.size == 1:
       Y_pred_start = np.array([Y_pred_start])
       Y_pred_end = np.array([Y_pred_end])
-    for i in range(Y_pred_start.shape[0]):
-      Y_pred_nt[Y_pred_start[i]:Y_pred_end[i]]=1
+    indices2 = [(Y_pred_start[i],Y_pred_end[i]) for i in range(len(Y_pred_start))]
+    doms = clustering(A=indices2, dist_max=dist_max, dom_min=dom_min)
+    for tupla in doms:
+      Y_pred_nt[tupla[0]:tupla[1]]=1
     TP_window = 0
     TP += 0
     FP += np.sum(Y_pred_nt)-TP_window
@@ -78,30 +82,18 @@ def per_domain(df_ltr, df_True, genoma, domain):
 
 
 def metrii(df_ltr, df_True, genoma, path_query):
-  df_ltr['real_start'] = df_ltr['Start']
-  df_ltr['real_end'] = df_ltr['Start']+df_ltr['Length']
-  print(df_ltr.columns)
-  print(df_ltr.head())
-
   if path_query == None:
     return 0, 0, 0, 0
 
   # Aquí se modifica el gff real para solo incluir dominios internos
+  df_True.reset_index(inplace = True)
+  print(df_True.head())
+  df_True = df_True.groupby('LTR_ID').agg({'Start': 'first', 'End': 'last', 'Chromosome': 'first'})
+  df_True.set_index('Chromosome', drop=True, inplace=True)
+  print(df_True.head())
   df_True['real_start'] = df_True['Start']
-  df_True['real_end'] = df_True['Start']+df_True['Length(bp)']
-
-  df_True['lon']=df_True['real_end']-df_True['real_start']
-  df_ltr['lon']=df_ltr['real_end']-df_ltr['real_start']
+  df_True['real_end'] = df_True['End']
 
   # Aquí se calculan las metricas
-  Precision_all, Recall_all, Accuracy_all, F1_all = per_domain(df_ltr, df_True, genoma, 'all')
-  Precision_GAG, Recall_GAG, Accuracy_GAG, F1_GAG = per_domain(df_ltr, df_True, genoma, 'GAG')
-  Precision_RT, Recall_RT, Accuracy_RT, F1_RT = per_domain(df_ltr, df_True, genoma, 'RT')
-  Precision_RNASEH, Recall_RNASEH, Accuracy_RNASEH, F1_RNASEH = per_domain(df_ltr, df_True, genoma, 'RNASEH')
-  Precision_INT, Recall_INT, Accuracy_INT, F1_INT = per_domain(df_ltr, df_True, genoma, 'INT')
-  Precision_AP, Recall_AP, Accuracy_AP, F1_AP = per_domain(df_ltr, df_True, genoma, 'AP')
-  Precision = [Precision_all, Precision_GAG, Precision_RT, Precision_RNASEH, Precision_INT, Precision_AP]
-  Recall = [Recall_all, Recall_GAG, Recall_RT, Recall_RNASEH, Recall_INT, Recall_AP]
-  Accuracy = [Accuracy_all, Accuracy_GAG, Accuracy_RT, Accuracy_RNASEH, Accuracy_INT, Accuracy_AP]
-  F1 = [F1_all, F1_GAG, F1_RT, F1_RNASEH, F1_INT, F1_AP]
+  Precision, Recall, Accuracy, F1 = per_domain(df_ltr, df_True, genoma)
   return Precision, Recall, Accuracy, F1
