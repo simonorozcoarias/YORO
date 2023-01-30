eb6606a312f38571f48f1f751c13c2705937fc00
johanpina <johanpina1420@gmail.com>
2023-01-30 08:15:00 -0500

    Changes to execute project in server

--- TestExecution.py
+++ TestExecution.py
@@ -1,15 +1,22 @@
 import os
+import pandas as pd
 import subprocess
 
 
 threads = 32
 
-index = range(0,41)
+data = pd.read_csv("metrics/genomes_links_final_final.csv",sep=';')
+
+numSamples = data.shape[0]
+print(f"El archivo tiene {numSamples} muestras")
+
+index = range(0,numSamples)
 
 for id in index:
     command = f'python -x {id} -D True -d test/ -p {threads}'
     try:
         print("#####################################################################")
+        print(command)
         subprocess.run(command)
         print("#########################¡¡DONE!!####################################")
     except:
--- job.sh
+++ job.sh
@@ -1,26 +1,18 @@
 #!/usr/bin/bash
 
-#SBATCH -J yoloDNA
+#SBATCH -J yolotests
 #SBATCH -D .
-#SBATCH -o out_yoloDNA.txt
-#SBATCH -e err_yoloDNA.txt
-#SBATCH -n 60
+#SBATCH -o out_yolotest.txt
+#SBATCH -e err_yolotest.txt
+#SBATCH -n 32
 #SBATCH -N 1
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:1
 #SBATCH --account=coffea_genomes
-#SBATCH --time=1-23:00:00
 #SBATCH --mem=300G
-####SBATCH --mail-type=ALL
-####SBATCH --mail-user=estiven.valenciac@autonoma.edu.co
 
 source ~/.bashrc
 unset PYTHONPATH
 conda activate YoloDNA
 
-#INGRESE LOS INDICES DE LAS SECUENCIAS QUE DESEA EJECUTAR
-for i in 188
-do
-    python3 pipelineDomain.py -t 0.5 -x $i
-    rm -f *.fasta
-done
+python TestExecution.py 
